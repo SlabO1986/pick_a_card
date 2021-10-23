8fbdb861d7281d83076406c10371b4a14d505694
SlabO10986 <42288250+SlabO10986@users.noreply.github.com>
2021-10-23 08:21:52 +0300

    Delete pick_a_card.rb

--- pick_a_card.rb
+++
@@ -1,5 +0,0 @@
-# enciding: utf-8
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] #Буби, Черви, Крести, Пики
-
-puts "#{values.sample} of #{suits.samole}"
